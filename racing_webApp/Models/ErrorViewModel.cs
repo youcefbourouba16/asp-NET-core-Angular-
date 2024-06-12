c9a5f55b33bd8dc1732fbe90701d3797f58d4498
youcefbourouba16 <109678299+youcefbourouba16@users.noreply.github.com>
2024-06-12 21:24:32 +0100

    Add project files.

---
+++ racing_webApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace racing_webApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
