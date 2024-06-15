1629afbe6cf172e818ec605e3d8a339fddc8bea8
youcefbourouba16 <109678299+youcefbourouba16@users.noreply.github.com>
2024-06-15 14:04:29 +0100

    .

--- racing app/racing_webApp/Controllers/ClubController.cs
+++ racing app/racing_webApp/Controllers/ClubController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using racing_webApp.Data;
+using racing_webApp.Models;
 
 namespace racing_webApp.Controllers
 {
@@ -15,6 +17,30 @@ namespace racing_webApp.Controllers
         {
             var clubs=_context.Clubs.ToList();
             return View(clubs);
+        }
+        public class ClubViewModel
+        {
+            public Club Club { get; set; }
+            public List<Club> RelatedClubs { get; set; } // Add this property
+        }
+        public IActionResult Details(int ID)
+        {
+
+            var club = _context.Clubs.Include(a => a.Address).FirstOrDefault(c => c.Id == ID);
+
+            // Fetch related clubs (This is just a sample. Adjust the logic as per your requirement)
+            var relatedClubs = _context.Clubs
+                                .Where(c => c.Id != ID && (c.ClubCategory==club.ClubCategory || c.Address==club.Address)) // Exclude the current club
+                                .Take(8) // Fetch 4 related clubs
+                                .ToList();
+
+            var viewModel = new ClubViewModel
+            {
+                Club = club,
+                RelatedClubs = relatedClubs // Set the related clubs
+            };
+
+            return View(viewModel);
         }
     }
 }
---
+++ racing app/racing_webApp/Controllers/RaceController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using racing_webApp.Data;
+using racing_webApp.Data.Enum;
+using racing_webApp.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace racing_webApp.Controllers
+{
+    public class RaceController : Controller
+    {
+        private readonly Db_context _context;
+
+        public RaceController(Db_context context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var Races = _context.Races.ToList();
+            return View(Races);
+        }
+
+        public class RaceViewModel 
+        {
+            public Race Race { get; set; }
+            public List<Race> RelatedRaces { get; set; } // Add this property
+        }
+        public IActionResult Details(int ID)
+        {
+
+            var race = _context.Races.Include(a => a.Address).FirstOrDefault(c => c.Id == ID);
+
+            // Fetch related clubs (This is just a sample. Adjust the logic as per your requirement)
+            var relatedClubs = _context.Races
+                                .Where(c => c.Id != ID && (c.RaceCategory ==race.RaceCategory || c.Address == race.Address)) // Exclude the current club
+                                .Take(8) // Fetch 4 related clubs
+                                .ToList();
+
+            var viewModel = new RaceViewModel
+            {
+                Race = race,
+                RelatedRaces = relatedClubs // Set the related clubs
+            };
+
+            return View(viewModel);
+        }
+    }
+}
