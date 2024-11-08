fac7fb57afca9dc7d5d51b8e548523a23c06319f
youcefbourouba16 <109678299+youcefbourouba16@users.noreply.github.com>
2024-11-08 22:07:27 +0100

    .

--- e-commerce/ShopingApi/ShopingApi/wwwroot/assets/js/main.js
+++ e-commerce/ShopingApi/ShopingApi/wwwroot/assets/js/main.js
@@ -164,42 +164,38 @@ jQuery(document).ready(function($) {
 	siteSliderRange();
 
 
-	var siteMagnificPopup = function () {
-		try {
-			$('.image-popup').magnificPopup({
-				type: 'image',
-				closeOnContentClick: true,
-				closeBtnInside: false,
-				fixedContentPos: true,
-				mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-				gallery: {
-					enabled: true,
-					navigateByImgClick: true,
-					preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-				},
-				image: {
-					verticalFit: true
-				},
-				zoom: {
-					enabled: true,
-					duration: 300 // don't forget to change the duration also in CSS
-				}
-			});
+	var siteMagnificPopup = function() {
+		$('.image-popup').magnificPopup({
+	    type: 'image',
+	    closeOnContentClick: true,
+	    closeBtnInside: false,
+	    fixedContentPos: true,
+	    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+	     gallery: {
+	      enabled: true,
+	      navigateByImgClick: true,
+	      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+	    },
+	    image: {
+	      verticalFit: true
+	    },
+	    zoom: {
+	      enabled: true,
+	      duration: 300 // don't foget to change the duration also in CSS
+	    }
+	  });
 
-			$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-				disableOn: 700,
-				type: 'iframe',
-				mainClass: 'mfp-fade',
-				removalDelay: 160,
-				preloader: false,
-				fixedContentPos: false
-			});
-		} catch (error) {
-			console.error("MagnificPopup initialization error:", error);
-		}
+	  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+	    disableOn: 700,
+	    type: 'iframe',
+	    mainClass: 'mfp-fade',
+	    removalDelay: 160,
+	    preloader: false,
+
+	    fixedContentPos: false
+	  });
 	};
 	siteMagnificPopup();
-
 
 
 });
